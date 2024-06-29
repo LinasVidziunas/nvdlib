77f97c823d4de8514cc79e32751a12ad75022bef
Linas Vidziunas <linasvidz@gmail.com>
2024-06-29 20:22:37 +0200

    🐛 fix: Retry on ReadTimeout
    - Wrapped request in try-except to handle ReadTimeout exceptions.
    - Enhanced retry mechanism with exponential backoff for timeouts.

--- nvdlib/get.py
+++ nvdlib/get.py
@@ -98,14 +98,19 @@ def __get_with_generator(product, headers, parameters, limit,
             print('Filter:\n' + link + stringParams)
         rate_delay = 1
         while True:
-            raw = requests.get(link, params=stringParams,
-                               headers=headers, timeout=30)
-            if raw.status_code == 403:
-                print(f'Request returned a rate limit error. Retrying in {rate_delay} seconds...')
+            try:
+                raw = requests.get(link, params=stringParams,
+                                   headers=headers, timeout=30)
+                if raw.status_code == 403:
+                    print(f'Request returned a rate limit error. Retrying in {rate_delay} seconds...')
+                    time.sleep(rate_delay)
+                    rate_delay *= 2
+                else:
+                    break
+            except requests.exceptions.ReadTimeout:
+                print(f'Request failed. Retrying in {rate_delay} seconds...')
                 time.sleep(rate_delay)
                 rate_delay *= 2
-            else:
-                break
 
         raw.encoding = 'utf-8'
         raw.raise_for_status()
